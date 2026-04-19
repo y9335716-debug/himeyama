ea8984708ef511eb6f818fa180b2d561b9e272c8
yuhi <y9335716@gmail.com>
2026-04-20 01:21:41 +0900

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,5 +1,11 @@
-fetch("nav.html")
-  .then(res => res.text())
-  .then(data => {
-    document.body.insertAdjacentHTML("afterbegin", data);
-  });
+<nav class="nav">
+  <div class="logo">姫山塾</div>
+
+  <div class="menu">
+    <a href="index.html">トップ</a>
+    <a href="features.html">特徴</a>
+    <a href="teacher.html">講師</a>
+    <a href="about.html">概要</a>
+    <a href="contact.html">問い合わせ</a>
+  </div>
+</nav>
